ce59b725622d5e5e9ee771cfbaadccf68e8c2e88
hermionearcher <hermione-archer@hotmail.co.uk>
2022-06-22 12:52:48 +0100

    3 files modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,17 @@ import React from 'react';
 export default function Header() {
   return (
     <section className="header">
-      <img className="header--img" src="./images/header-gallery.png"/>
+      <img
+        className="header--img"
+        src="https://stackblitz.com/files/react-ls7hs6/github/hermionearcher/airbnb-experiences-clone/master/src/Group 77.png"
+      />
       <div className="header--text">
         <h1>Online Experiences</h1>
-        <p>Join unique interactive activites led by one-of-a-kind hosts -all without leaving home.</p>
+        <p>
+          Join unique interactive activites led by one-of-a-kind hosts -all
+          without leaving home.
+        </p>
       </div>
     </section>
-  )
+  );
 }
